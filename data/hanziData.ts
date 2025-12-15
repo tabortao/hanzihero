f83afc1386f4c17113a21c793d7f404e8b2e0bc3
T先生分享 <815141681@qq.com>
2025-12-15 09:56:36 +0800

    refactor: Remove deprecated hanzi data files
    The `hanziData.ts` and `data/hanziData.ts` files, as well as the `data.ts` file containing curriculum data, are no longer used and have been replaced by imports from `data/dictionary/index.ts`. These files can be safely deleted.

--- data/hanziData.ts
+++
@@ -1,1 +0,0 @@
-// This file is deprecated. Please use data/dictionary/index.ts
--- hanziData.ts
+++
@@ -1,3 +0,0 @@
-
-// This file is deprecated and its contents have been moved to data/dictionary/index.ts
-// It can be safely deleted.
